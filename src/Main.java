cf0d3d80e2c86cb038ba80a9b08dcb3c192b15c9
Mohamed-Farraj <mohamedfarraj27@gmail.com>
2025-07-05 16:42:35 +0300

    make a use case for program

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,23 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+        shippableProduct tv = new shippableProduct("TV", 5000, 5, 7000);
+        shippableProduct cheese = new shippableProduct("Cheese", 100, 10, 400);
+        expirableProduct biscuits = new expirableProduct("Biscuits", 50, 20, LocalDate.now().plusDays(5));
+        simpleProduct scratchCard = new simpleProduct("Scratch Card", 20, 100) {};
+
+
+        customer c1 = new customer("Mohamed", 12000);
+
+
+        c1.addToCart(tv, 2);
+        c1.addToCart(cheese, 3);
+        c1.addToCart(biscuits, 5);
+        c1.addToCart(scratchCard, 4);
+
+
+        Checkout checkoutService = new Checkout();
+        checkoutService.checkout(c1);
     }
 }
